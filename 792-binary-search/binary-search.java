1d9d75ec25b90ce29b4db6521ac99d7381476974
Rachit Saini <rachit.rrr2005@gmail.com>
2025-08-13 22:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (60.90%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,11 +4,11 @@ class Solution {
         int end = nums.length - 1;
         while(start <= end){
             int mid = start + (end - start) / 2;
-            if(nums[mid] == target){
+            if(target == nums[mid]){
                 return mid;
             } else if(target < nums[mid]){
                 end = mid - 1;
-            } else if(target > nums[mid]){
+            } else{
                 start = mid + 1;
             }
         }
