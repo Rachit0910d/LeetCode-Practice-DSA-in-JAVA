afe3c5a3f255bf0ed586d2211b1a689bb27ec2dd
Rachit Saini <rachit.rrr2005@gmail.com>
2025-08-14 22:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (81.58%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -3,7 +3,6 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int count = 0;
         int sRow = 0;
         int sCol = 0;
         int eRow = m-1;
@@ -12,25 +11,21 @@ class Solution {
         while(sRow <= eRow && sCol <= eCol){
             for(int j = sCol; j <= eCol && (sRow <= eRow && sCol <= eCol); j++){
                 list.add(matrix[sRow][j]);
-                count++;
             }
             sRow++;
 
             for(int i = sRow; i <= eRow && (sRow <= eRow && sCol <= eCol); i++){
                 list.add(matrix[i][eCol]);
-                count++;
             }
             eCol--;
 
             for(int j = eCol; j >= sCol && (sRow <= eRow && sCol <= eCol); j--){
                 list.add(matrix[eRow][j]);
-                count++;
             }
             eRow--;
 
             for(int i = eRow; i >= sRow && (sRow <= eRow && sCol <= eCol); i--){
                 list.add(matrix[i][sCol]);
-                count++;
             }
             sCol++;
         }
