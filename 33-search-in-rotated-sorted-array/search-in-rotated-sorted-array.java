13c5b9de2ae1e096788f7e573fde9265f1ce412f
Rachit Saini <rachit.rrr2005@gmail.com>
2026-09-05 15:52:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (65.87%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,63 +1,61 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length == 1){    // if array length is one then check directly
-            if(nums[0] == target){
-                return 0;
-            } else{
-                return -1;
-            }
-        }
-        int pivot = findingPivot(nums);   // find pivot element
+        int pivot = findPivotIndex(nums);
 
-        if(pivot == -1){    // if pivot is not found then do normal binary search
-            return firstHalfTry(nums, target, 0, nums.length - 1);
+        if(pivot == -1){
+            return findInHalfTry(nums, 0, nums.length - 1,  target);
         }
 
-        if(nums[pivot] == target) return pivot; // if pivot element == to the target then return pivot
-
-        int ans = firstHalfTry(nums, target, 0, pivot - 1);  // finding target element in the first half array
-        if(ans != -1){
-            return ans;   // if you find in first half array then return the index
-        } else{
-            return firstHalfTry(nums, target, pivot + 1, nums.length - 1);  // if you do not find in the first half array then check in the second half array
+        if(nums[pivot] == target){
+            return pivot;
         }
+
+        int ans = findInHalfTry(nums, 0, pivot - 1, target);
+
+        if(ans == -1){
+            ans = findInHalfTry(nums, pivot + 1, nums.length - 1, target);
+        }
+
+        return ans;
     }
 
-     int findingPivot(int[] arr){
-        int start = 0;
-        int end = arr.length - 1;
+    private int findInHalfTry(int[] nums, int s, int e, int target){
+        while(s <= e){
+            int m = s + (e - s) / 2;
 
-        if(arr.length == 0) return -1;
-
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(mid < end && arr[mid] > arr[mid + 1]){
-                    return mid;
-            }
-            if( mid > start && arr[mid] < arr[mid -1]){
-                return mid - 1;
-            }
-            if(arr[start] >= arr[mid]){
-                end = mid - 1;
+            if(nums[m] == target){
+                return m;
+            } else if(nums[m] > target){
+                e = m - 1;
             } else{
-                start = mid + 1;
+                s = m + 1;
             }
         }
         return -1;
     }
 
-     int firstHalfTry(int[] arr, int target, int start, int end){
-        while(start <= end){
-            int mid = start + (end - start) / 2;
+    private int findPivotIndex(int[] nums){
+        if(nums.length == 0){
+            return -1;
+        }
 
-            if(target < arr[mid]){
-                end = mid - 1;
-            } else if(target > arr[mid]){
-                start = mid + 1;
-            } else {
-                return mid;
+        int l = 0;
+        int r = nums.length - 1;
+
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if(m < r && nums[m] > nums[m + 1]){
+                return m;
+            } else if(m > l && nums[m] < nums[m - 1]){
+                return m - 1;
+            } else if(nums[l] >= nums[m]){
+                r = m - 1;
+            } else{
+                l = m + 1;
             }
         }
+
         return -1;
     }
+
 }
