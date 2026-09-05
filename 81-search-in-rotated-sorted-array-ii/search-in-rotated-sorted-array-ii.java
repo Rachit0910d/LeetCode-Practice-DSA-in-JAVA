3cc883de88c4922686a80a4e975becfe287682c1
Rachit Saini <rachit.rrr2005@gmail.com>
2026-09-05 16:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (65.65%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,23 +1,38 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        if(nums[0] == target){
-            return true;
-        }
-        Arrays.sort(nums);
+        int n = nums.length;
+       int low = 0, high = n-1;
 
-        int start = 0;
-        int end = nums.length - 1;
+       while (low <= high) {
+            int mid = (low+high)/2;
+            if (nums[mid] == target) return true;
 
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target){
-                return true;
-            } else if(nums[mid] > target){
-                end = mid - 1;
-            } else{
-                start = mid + 1;
+            if (nums[low]==nums[mid] && nums[mid]==nums[high]) {
+                low++;
+                high--;
+                continue;
             }
+
+            // check if left half is sorted
+            if (nums[low] <= nums[mid]) {
+                // check if the target lies in the sorted left half
+                if (nums[low]<=target && target<=nums[mid]) {
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+            }
+            // Otherwise, the right half is sorted
+            else {
+                //check if the target lies in the sorted right half
+                if (nums[mid]<=target && target<=nums[high]) {
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+        }
         }
         return false;
     }
+
 }
