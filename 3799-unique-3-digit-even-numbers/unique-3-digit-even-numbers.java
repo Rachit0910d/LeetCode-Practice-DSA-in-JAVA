1b7298181efde77945738b461faf5aa3cec78875
Rachit Saini <rachit.rrr2005@gmail.com>
2026-09-11 18:16:08 +0530

    Time: 6 ms (50.40%) | Memory: 46.5 MB (35.33%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -3,14 +3,14 @@ class Solution {
         Set<Integer> unique = new HashSet<>();
 
         for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == 0) continue; // no leading zero
+            if (digits[i] == 0) continue; 
 
             for (int j = 0; j < digits.length; j++) {
                 if (j == i) continue;
 
                 for (int k = 0; k < digits.length; k++) {
                     if (k == i || k == j) continue;
-                    if (digits[k] % 2 != 0) continue; // must be even
+                    if (digits[k] % 2 != 0) continue; 
 
                     int number = digits[i] * 100 + digits[j] * 10 + digits[k];
                     unique.add(number);
