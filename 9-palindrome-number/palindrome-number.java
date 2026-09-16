93779eab190136d52ce9f925432ea5f1c02824d3
Rachit Saini <rachit.rrr2005@gmail.com>
2026-09-16 21:18:54 +0530

    Time: 5 ms (83.02%) | Memory: 46 MB (33.17%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+
+        int y = x;
+        int ans = 0;
+
+        while(y !=0){
+            int i = y % 10;
+            ans = ans*10 + i;
+            y /= 10;
+        }
+
+        if(ans == x){
+            return true;
+        }
+
+        return false;
+
+
+    }
+}
